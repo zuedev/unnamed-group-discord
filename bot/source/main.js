2f0b751c5490ef0c3f9e76ef0ab05b8526d29567
Alex Pooley (@zuedev) <zuedev@gmail.com>
2025-08-07 00:08:38 +0100

    refactor: remove unused MongoDB and OpenAI controllers, clean up main.js

--- bot/source/main.js
+++ bot/source/main.js
@@ -5,23 +5,10 @@ import {
   Partials,
   ActivityType,
   Events,
-  MessageType,
   PermissionFlagsBits,
   ChannelType,
 } from "discord.js";
 import registerSlashCommands from "./library/registerSlashCommands.js";
-import { upsertOne } from "./controllers/mongodb.js";
-import { chat } from "./controllers/openai.js";
-import * as Sentry from "@sentry/node";
-import { nodeProfilingIntegration } from "@sentry/profiling-node";
-import { flag } from "./controllers/unleash.js";
-
-Sentry.init({
-  dsn: process.env.SENTRY_DSN,
-  integrations: [nodeProfilingIntegration()],
-  tracesSampleRate: 1.0,
-  profilesSampleRate: 1.0,
-});
 
 const discord = new Client({
   intents: Object.values(GatewayIntentBits),
@@ -99,90 +86,6 @@ discord.on(Events.InteractionCreate, async (interaction) => {
       });
     }
   }
-});
-
-discord.on(Events.MessageCreate, async (message) => {
-  // ignore events from other guilds not specified in the .env file
-  if (message.guild?.id !== process.env.DISCORD_GUILD_ID) return;
-
-  if (message.author.bot) return;
-
-  if (message.type === MessageType.Reply)
-    message._replyMessage = await message.channel.messages.fetch(
-      message.reference.messageId
-    );
-
-  if (
-    message.content.startsWith(`<@${discord.user.id}>`) ||
-    flag("chatty") ||
-    Math.random() < 0.01 ||
-    message._replyMessage?.author.id === discord.user.id
-  ) {
-    let replyContent;
-
-    replyContent = message.content;
-
-    if (message.content.startsWith(`<@${discord.user.id}>`))
-      replyContent = replyContent.replace(`<@${discord.user.id}>`, "");
-
-    await message.reply({
-      content: await chat({
-        message: replyContent,
-        image_url: message.attachments.first()?.url,
-      }),
-    });
-  }
-
-  // increment the message count for the author
-  await upsertOne(
-    "members",
-    { discordId: message.author.id },
-    {
-      $set: {
-        discordId: message.author.id,
-        discordLastKnownTag: message.author.tag,
-      },
-      $inc: {
-        discordMessageCount: 1,
-      },
-    }
-  );
-
-  console.log(
-    `${message.author.tag} sent a message in "${message.guild.name}'s" #${message.channel.name} channel. Incremented message count by 1 in the database.`
-  );
-});
-
-discord.on(Events.MessageDelete, async (message) => {
-  // if the message is partial, ignore it as we can't fetch the message author anyway
-  if (message.partial)
-    return console.log(
-      "A partial message was deleted. Ignoring MessageDelete event."
-    );
-
-  // ignore events from other guilds not specified in the .env file
-  if (message.guild?.id !== process.env.DISCORD_GUILD_ID) return;
-
-  if (message.author.bot) return;
-
-  // decrement the message count for the author
-  await upsertOne(
-    "members",
-    { discordId: message.author.id },
-    {
-      $set: {
-        discordId: message.author.id,
-        discordLastKnownTag: message.author.tag,
-      },
-      $inc: {
-        discordMessageCount: -1,
-      },
-    }
-  );
-
-  console.log(
-    `${message.author.tag}'s message in "${message.guild.name}'s" #${message.channel.name} channel was deleted. Decremented message count by 1 in the database.`
-  );
 });
 
 discord.on(Events.VoiceStateUpdate, async (oldState, newState) => {
